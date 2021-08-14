ac7278842c889a37f4902ac09084186ea7cb6da5
Zainabraja <zainabraja53@gmail.com>
2021-08-14 14:22:48 +0530

    mycommiy

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.get('/', function (req, res) {
 });
 
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000!');
-});
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`)
+})
